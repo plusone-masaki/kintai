501936a88a509c12f794405c8e5f49dddad4e9a2
m_enomoto <m_enomoto@plus-one.tech>
2021-09-24 13:51:51 +0900

    # ヘルパが二重になっていたのを修正 - Formatter.tsにまとめた

--- src/helpers/Formatter.ts
+++ src/helpers/Formatter.ts
@@ -1,5 +1,6 @@
 const Formatter = {
   leftPad: (str: string|number, maxLength: number, fillString: string) => str.toString().padStart(maxLength, fillString),
+  numberFormat: (value: string|number): string => new Intl.NumberFormat('ja-JP').format(Number(value)),
 }
 
 export default Formatter
--- src/plugins/helpers.ts
+++
@@ -1,1 +0,0 @@
-export const numberFormat = (value: string|number): string => new Intl.NumberFormat('ja-JP').format(Number(value))
