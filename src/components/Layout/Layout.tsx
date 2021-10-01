69f3e1f84a2e4ff0dfc2254479bb8e16782a4338
m_enomoto <m_enomoto@plus-one.tech>
2021-10-01 18:51:13 +0900

    # タイトルをセット - next/head を使ってページタイトルをセットした

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
+import Head from 'next/head'
 import Header from './Header'
+import { t } from '@/helpers/WordManager'
 
 type Props = {
     children?: ReactNode
@@ -7,9 +9,14 @@ type Props = {
 
 const Layout = ({ children }: Props) => (
   <>
+    <Head>
+      <title>{ t('common.app_name') }</title>
+    </Head>
+
     <Header />
+
     <main>
-      {children}
+      { children }
     </main>
   </>
 )
